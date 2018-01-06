50f256e31e30baaa7d6e6c013301c09f8db878f4
Long.Nguyen <long.nguyen@bacsitoancau.com>
2018-01-06 08:28:40 +0700

    Init

---
+++ CustomCamera/CustomCamera/Models/IconView.swift
@@ -0,0 +1,38 @@
+//
+//  IconView.swift
+//  CustomCamera
+//
+//  Created by NguyenDinh.Long on 1/5/18.
+//  Copyright © 2018 FAYA Corporation. All rights reserved.
+//
+
+import UIKit
+
+class IconView: UIImageView {
+
+  private var originalCenter: CGPoint?
+  private var dragStart: CGPoint?
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    originalCenter = center
+//    dragStart = touches.first!.location(in: superview)
+    dragStart = self.frame.origin
+    
+  }
+  
+  override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    var location = touch.location(in: superview)
+    if let predicted = event?.predictedTouches(for: touch)?.last {
+      location = predicted.location(in: superview)
+    }
+    center = dragStart! + location - originalCenter!
+  }
+  
+  override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    guard let touch = touches.first else { return }
+    let location = touch.location(in: superview)
+    center = dragStart! + location - originalCenter!
+  }
+
+}
