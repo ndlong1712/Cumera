9e6c1dbd599a42f365e38388dfaf9097b8bafa05
Tien Nguyen <tiennt7@fsoft.com.vn>
2018-03-28 01:29:17 +0700

    Add gitignore

---
+++ CustomCamera/CustomCamera/ViewControllers/CodeVerificationController.swift
@@ -0,0 +1,35 @@
+//
+//  CodeVerificationController.swift
+//  Cheese
+//
+//  Created by Tien on 3/27/18.
+//  Copyright © 2018 FAYA Corporation. All rights reserved.
+//
+
+import UIKit
+
+class CodeVerificationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CustomCamera/CustomCamera/Views/BorderedLabel.swift
@@ -0,0 +1,21 @@
+//
+//  BorderedLabel.swift
+//  Cheese
+//
+//  Created by Tien on 3/27/18.
+//  Copyright © 2018 FAYA Corporation. All rights reserved.
+//
+
+import UIKit
+
+class BorderedLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
